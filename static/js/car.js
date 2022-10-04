ddc85cc58c1b008962b17c35161b34bd0795a7ff
jacob miller <jacobmiller0@protonmail.ch>
2022-10-04 18:25:53 +1300

    here we go again

---
+++ static/js/car.js
@@ -0,0 +1,73 @@
+class Car {
+    constructor(x,y,width,hight){
+        this.x = x
+        this.y = y
+        this.width = width
+        this.hight = hight
+
+        this.speed = 0
+        this.accesraton = 0.2;
+        this.maxSpeed = 9;
+        this.frick = 0.05;
+        this.diret = 0
+
+        this.controls = new Controls
+    }
+
+    update(){
+        this.#move()
+    }
+
+    #move(){
+        if(this.controls.forward){
+            this.speed +=this.accesraton
+        }
+        if(this.controls.back){
+            this.speed -=this.accesraton
+        }
+
+        if(this.speed>this.maxSpeed){
+            this.speed = this.maxSpeed
+        }
+        if(this.speed<-this.maxSpeed/2){
+            this.speed = -this.maxSpeed/2
+        }
+        if(this.speed>0){
+            this.speed -= this.frick
+        }
+        if(this.speed<0){
+            this.speed += this.frick
+        }
+        if(Math.abs(this.speed)<this.frick){
+            this.speed = 0
+        }
+        if (this.speed != 0){
+            if(this.controls.left){
+                this.diret += 0.03
+            }
+            if(this.controls.right){
+                this.diret-=0.03
+            }
+        }
+
+        this.x-=Math.sin(this.diret)*this.speed
+        this.y-=Math.cos(this.diret)*this.speed
+    }
+
+    draw(ctx){
+        ctx.save();
+        ctx.translate(this.x,this.y);
+        ctx.rotate(-this.diret)
+        ctx.beginPath();
+        ctx.rect(
+            -this.width/2,
+            -this.hight/2,
+            this.width,
+            this.hight
+        );
+        ctx.fill();
+
+        ctx.restore();
+    }
+
+}
---
+++ static/js/controls.js
@@ -0,0 +1,46 @@
+class Controls{
+    constructor(){
+        this.forward = false
+        this.left = false
+        this.right = false
+        this.back = false
+
+        this.#addKeyControls();
+    }
+
+    #addKeyControls(){
+        document.onkeydown=(event)=>{
+            switch(event.key){
+                case "ArrowLeft":
+                    this.left = true;
+                    break;
+                case "ArrowRight":
+                    this.right = true;
+                    break;
+                case "ArrowUp":
+                    this.forward = true;
+                    break;
+                case "ArrowDown":
+                    this.back = true;
+                    break;
+            }
+        }
+        document.onkeyup=(event)=>{
+            switch(event.key){
+                case "ArrowLeft":
+                    this.left = false;
+                    break;
+                case "ArrowRight":
+                    this.right = false;
+                    break;
+                case "ArrowUp":
+                    this.forward = false;
+                    break;
+                case "ArrowDown":
+                    this.back = false;
+                    break;
+            }
+            console.table(this);
+        }
+    }
+}
